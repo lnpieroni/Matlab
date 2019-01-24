401c52dcaac7d2f1ea32c570134122f7ddd072c3
lnpieroni <l4cuatro@gmail.com>
2019-01-24 15:19:12 -0500

    Actually done with 1/24 this time

--- Participation/hwAnalysis.m
+++ Participation/hwAnalysis.m
@@ -12,17 +12,26 @@ filename = "HomeworkData.txt";
 hwRawData = load(filename);
 
 assignmentNum = input("Enter Assignment #");
+studentNum = input("Enter Student #");
 
 %% Calculate mean & standard deviation
 
-means = mean(hwRawData); % Average score of each assignment
-stdevs = std(hwRawData); % Standard deviation of each assignment
+assMeans = mean(hwRawData); % Average score of each assignment
+assDevs = std(hwRawData); % Standard deviation of each assignment
+
+studentMeans = mean(hwRawData, 2);
+studentDevs = std(hwRawData, 0, 2);
 
 %% Output
 
 out = "The mean score for assignment #" + num2str(assignmentNum)...
-    + " is " + num2str(means(assignmentNum)) + "%" + char(10)...
+    + " is " + num2str(assMeans(assignmentNum)) + "%" + char(10)...
     + "The standard deviation of assignment #" + num2str(assignmentNum)...
-    + " is " + num2str(stdevs(assignmentNum));
+    + " is " + num2str(assDevs(assignmentNum)) + char(10)...
+    + "The mean score for student #" + num2str(studentNum)...
+    + " is " + num2str(studentMeans(assignmentNum)) + "%" + char(10)...
+    + "The standard deviation of assignment #" + num2str(assignmentNum)...
+    + " is " + num2str(studentDevs(assignmentNum));
+
 
 disp(out);

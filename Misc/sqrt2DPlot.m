17127bc7fb08bdc4f15f0c267ff51cbac4cfc4b4
lnpieroni <l4cuatro@gmail.com>
2019-02-06 21:34:42 -0500

    sqrt2DPlot.m is no longer a square root function

--- Misc/sqrt2DPlot.m
+++ Misc/sqrt2DPlot.m
@@ -5,17 +5,39 @@
 clear;
 clc;
 
-lStick = -127:127;
-rStick = [-127; -63; 0; 63; 127];
+lStick = linspace(-1, 1, 100);
+rStick = [-1; -.75; -.5; 0; .5; .75; 1];
 
-inputs = lStick + rStick;
+alpha = .25;
+beta = 0.25;
+gamma = 0.6;
 
-mtrPwr = sqrt(abs(inputs)) .* sign(inputs);
+lInsForward = (lStick.^3 + lStick.*(alpha))./(1+alpha);
+rInsForward = (rStick.^3 + rStick.*(alpha))./(1+alpha);
 
-plotFormat = ["b-o", "r-x", "g-+", "c-d", "m-*"];
+lInsTurn = (lStick.^3 + lStick.*(beta))./(1+beta);
+rInsTurn = (rStick.^3 + rStick.*(beta))./(1+beta);
+
+lMtrPwr = lInsForward + (rInsTurn).*(1-abs(lInsForward.*gamma));
+rMtrPwr = lInsForward - (rInsTurn).*(1-abs(lInsForward.*gamma));
+
+lMtrPwr = min(max(lMtrPwr, -1), 1);
+rMtrPwr = min(max(rMtrPwr, -1), 1);
+
+plotFormat = ["b-", "r-", "g-", "c-", "m-", "y-", "k-"];
 
 hold on;
-for iPlotCt = 1:5
-    plot(lStick, mtrPwr(iPlotCt, :), plotFormat(iPlotCt));
+for iPlotCt = 1:7
+    plot(lStick, lMtrPwr(iPlotCt, :), plotFormat(iPlotCt));
 end
+% figure('Name', "Left Motor");
 legend(num2str(rStick));
+hold off;
+figure;
+hold on;
+for iPlotCount = 1:7
+    plot(lStick, rMtrPwr(iPlotCount, :), plotFormat(iPlotCount));
+end
+% figure('Name', "Right Motor");
+legend(num2str(rStick));
+hold off;
